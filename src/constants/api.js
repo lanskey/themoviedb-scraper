41099467dca4fbebf3b1982787211aceee046191
Emanuel Slotwinski <ask@eslotwinski.com>
2016-12-16 02:31:26 +0100

    Simple api constants

---
+++ src/constants/api.js
@@ -0,0 +1,11 @@
+const baseUrl = 'http://api.themoviedb.org/3'
+const apiKey = 'api_key=9dee05d48efe51f51b15cc63b1fee3f5'
+const endpoint = '/discover/movie'
+const route = `${baseUrl}${endpoint}?${apiKey}`;
+
+module.exports = {
+  baseUrl,
+  route,
+  endpoint,
+  apiKey
+}
