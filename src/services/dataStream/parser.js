706bb37ca1cf7691800b46e0be8148c63baf6a13
Emanuel Slotwinski <ask@eslotwinski.com>
2017-01-04 00:05:39 +0100

    DataStream returns it's constructor

--- src/services/dataStream/parser.js
+++ src/services/dataStream/parser.js
@@ -1,34 +1,10 @@
 // Klasa to logiczny zbior funkcji(metod) i danych (proporcji)
 // klasa to "cos"
 
-const events = require('events')
-const eventEmitter = new events.EventEmitter()
-const nodemon = require('nodemon')
+class DataStream {
+  constructor() {
 
-const { logger, getDate } = require('utils/helpers')
-const callApi = require('../callApi')
-
-let counter = 0
-const getMovies = function () {
-  const url = 'http://api.themoviedb.org/3/discover/movie?api_key=9dee05d48efe51f51b15cc63b1fee3f5'
-
-  return callApi(url)
-    .then((data) => {
-      logger.info(`${getDate()} API call has been done`)
-      counter += 1
-      // data.results.forEach(item => database.push(item))
-      console.log(counter)
-      if (counter === 3) {
-        nodemon.emit('quit')
-        process.exit()
-      }
-      eventEmitter.emit('getMoviesCompleted')
-      return data
-    })
-    .catch(err => logger.error(err))
+  }
 }
 
-eventEmitter.on('getMoviesCompleted', getMovies)
-eventEmitter.on('getMovies', getMovies)
-
-module.exports = eventEmitter
+module.exports = DataStream
--- src/services/dataStream/test/parser.test.js
+++ src/services/dataStream/test/parser.test.js
@@ -1,7 +1,20 @@
-const callApi = require('../parser')
+const Parser = require('../parser')
 
 describe('DataStream', () => {
-  describe('parser', () => {
-    // tests
+  describe('Constructor', () => {
+    let defaults = {}
+    before(() => {
+      defaults = {
+        apiKey: null,
+        limit: 3,
+        url: 'http://api.themoviedb.org/3',
+        endPoint: '/discover/movie'
+      }
+    })
+
+    it('create new Constructor instance', () => {
+      const client = new Parser()
+      expect(client).to.be.instanceof(Parser)
+    })
   })
 })
